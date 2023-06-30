e4f16638800fba10b76c58d0c172a0b74fcef0d3
IASTATE\rebekahy <rebekahy@iastate.edu>
2023-06-30 15:17:46 -0500

    Added function to buttons

--- Assets/Scripts/SceneSwiitcher.cs
+++ Assets/Scripts/SceneSwiitcher.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class SceneSwiitcher : MonoBehaviour
 {
+    [SerializeField]
+    GameObject[] Menus;
+
+    int iterator = 0;
+
+    public void ActivateMenu(int menuNumber) { 
+        foreach (GameObject menu in Menus)
+        {
+            menu.SetActive(false); //Turns everything off
+        }
+        Menus[menuNumber].SetActive(true); //Turns on desired menu
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        ActivateMenu(0);
     }
-
-    // Update is called once per frame
-    void Update()
+    public void InterateMenu()
     {
-        
+        iterator++; //Adds one
+        ActivateMenu(iterator);
     }
 }
